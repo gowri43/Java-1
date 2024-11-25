243f85ab1f95086e2f7a9188e263f87477f65661
Gowri <Gowri@192.168.1.10>
2024-11-25 15:35:48 +0530

    created java variable

---
+++ Java/src/java_datatypes_variables/JavaVariables.java
@@ -0,0 +1,13 @@
+package java_datatypes_variables;
+
+public class JavaVariables {
+
+	public static void main(String[] args) {
+
+		String myName = "Gowri";
+
+		System.out.println(myName);
+
+	}
+
+}
