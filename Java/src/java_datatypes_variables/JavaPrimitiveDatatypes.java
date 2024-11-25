1f8755a07eef80bd046da7ca977f55e0e6994924
Gowri <Gowri@192.168.1.10>
2024-11-25 15:36:11 +0530

    created java primitive data types

---
+++ Java/src/java_datatypes_variables/JavaPrimitiveDatatypes.java
@@ -0,0 +1,33 @@
+package java_datatypes_variables;
+
+public class JavaPrimitiveDatatypes {
+
+	public static void main(String[] args) {
+
+		int myAge = 29;
+		long myPhoneNumber = 9876543210L;
+
+		float myHeight = 175.5f;
+		double mySalary = 65000.88d;
+
+		char myInitial = 'S';
+		boolean isEmployee = true;
+
+		byte myLuckyNumber = 6;
+		short myLifePathNumber = 11;
+
+		System.out.println("My Personal Details: ");
+		System.out.println();
+
+		System.out.println(" My Age is: " + myAge);
+		System.out.println(" My Phone Number is: " + myPhoneNumber);
+		System.out.println(" My Height is:  " + myHeight);
+		System.out.println(" My Salary is:  " + mySalary);
+		System.out.println(" My Initial is: " + myInitial);
+		System.out.println(" Is an Employee: " + isEmployee);
+		System.out.println(" My Lucky Number is: " + myLuckyNumber);
+		System.out.println(" My LifePath Number is: " + myLifePathNumber);
+
+	}
+
+}
