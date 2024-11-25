4b88f9c0a61a2fe3985e6c56c9f314014f89e748
Gowri <Gowri@192.168.1.10>
2024-11-25 16:07:33 +0530

    created single line comment in java

---
+++ Java/src/javacomments/SingleLineComment.java
@@ -0,0 +1,12 @@
+package javacomments;
+
+public class SingleLineComment {
+
+	public static void main(String[] args) {
+
+		// single line comment
+
+		System.out.println("Single line comment in java is denoted by " + " // double forward slash");
+	}
+
+}
