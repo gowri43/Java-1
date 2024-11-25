18408bb888d68c670274f3c589523e85640fa418
Gowri <Gowri@192.168.1.10>
2024-11-25 16:07:47 +0530

    created multi line comments in java

---
+++ Java/src/javacomments/MultiLineComments.java
@@ -0,0 +1,17 @@
+package javacomments;
+
+public class MultiLineComments {
+
+	public static void main(String[] args) {
+
+		/**
+		 * 
+		 * Multi line comments
+		 * 
+		 * 
+		 * 
+		 */
+		System.out.println("Multiline comment is denoted by /***/ ");
+	}
+
+}
